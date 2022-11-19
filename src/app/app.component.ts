f4801c8225e7301e97fc6fe9cd0cf1fb59e1163b
GuilhermeCostaDF <guidocostadf@gmail.com>
2022-11-19 15:29:13 -0300

    primeiro commit; metodo implementado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,12 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import {CdkDragDrop, moveItemInArray, transferArrayItem} from '@angular/cdk/drag-drop'
+
+export interface Character{
+  nome: string
+  image: string
+  fatality: string
+}
 
 @Component({
   selector: 'app-root',
@@ -7,4 +15,32 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'lista-dinamica';
+
+  myList: Character[] = [];
+  confirmList: Character[] =[];
+
+  constructor(private httpClient: HttpClient){
+    this.getMyList()
+  }
+
+  getMyList(){
+    this.httpClient.get<Character[]>('../assets/data.json')
+    .subscribe(list => this.myList = list)
+  }
+
+  drop(event: CdkDragDrop<Character[]>){
+    //verifica qual lista
+    if(event.previousContainer == event.container){
+      // movendo na mesma lista
+      moveItemInArray(event.container.data,
+        event.previousIndex,
+        event.currentIndex)
+    }else{
+      //transferindo item para outra lista
+      transferArrayItem(event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex);
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 
+import {DragDropModule} from '@angular/cdk/drag-drop'
+import { HttpClientModule } from '@angular/common/http';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    DragDropModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
